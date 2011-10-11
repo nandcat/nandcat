02c24bb4f9b9a2257b1d6099550a94592e6e8fdd
Benedikt Lang <github@benediktlang.de>
2011-10-11 18:38:10 +0200

    Main kommentiert.

--- src/main/java/de/unipassau/sep/nandcat/Nandcat.java
+++ src/main/java/de/unipassau/sep/nandcat/Nandcat.java
@@ -1,5 +1,8 @@
 package de.unipassau.sep.nandcat;
 
+import javax.swing.SwingUtilities;
+
+import de.unipassau.sep.nandcat.model.Model;
 
 /**
  * NandCat Main Class.
@@ -8,14 +11,40 @@ package de.unipassau.sep.nandcat;
  * 
  */
 public class Nandcat {
-    // private View view;
-    // private Model model;
-    // private Controller controller;
-    // TODO Creates model, view, controller but only holds instances
-    // temporarily, no fields.
+	// private View view;
+	// private Model model;
+	// private Controller controller;
+	// TODO Creates model, view, controller but only holds instances
+	// temporarily, no fields.
 
-    public static void main(String[] args) {
-
-    }
+	/**
+	 * 
+	 */
+	private Nandcat(){
+		 throw new UnsupportedOperationException();
+	}
+	
+	/**
+	 * Main method, executed while application start.
+	 * 
+	 * Starts Swingthread and application.
+	 * @param args Command-line arguments.
+	 */
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				Nandcat.initApp();
+			}
+		});
+	}
+	
+	/**
+	 * Init application.
+	 */
+	private static void initApp(){
+		Model model = new Model();
+		// TODO View und Controller entsprechend initialisieren und Konstruktoren schreiben.
+		
+	}
 
 }
