16635d7dcc7cf93d0a5202347f65e8bae6756334
Veronika Scholz <veronikascholz@gmail.com>
2011-10-16 15:51:16 +0200

    Vorläufige Milestones festgelegt - Nach Funktionen und Klassen aufgeteilt - Tools für Import/Export und Delete zugefügt

---
+++ src/main/java/de/unipassau/sep/nandcat/controller/ImportExportTool.java
@@ -0,0 +1,36 @@
+package de.unipassau.sep.nandcat.controller;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+
+/**
+ * Tool for importing and exporting files. Includes saving and loading.
+ * @author vroni
+ *
+ */
+public class ImportExportTool implements Tool {
+
+    // TODO maybe split into two classes.
+
+    public void setActive(boolean active) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public ActionListener getListener() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getText() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public ImageIcon getIcon() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
