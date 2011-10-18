38c33b7829f9553128ba1ecb3da7ae400f74e196
Lorentz Roßbacher <lancetekk@googlemail.com>
2011-10-18 20:45:15 +0200

    checks hinzugefügt!

---
+++ src/main/java/nandcat/model/check/IllegalConnectionCheck.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package nandcat.model.check;
+
+import nandcat.model.element.Circuit;
+
+/**
+ * Checks if all connections only connect in-Ports with out-Ports.
+ */
+public class IllegalConnectionCheck {
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean isActive() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean setActive(boolean active) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean test(Circuit circuit) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void addListener(CheckListener l) {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void removeListener(CheckListener l) {
+        // TODO Auto-generated method stub
+    }
+}
---
+++ src/main/java/nandcat/model/check/MultipleConnectionsCheck.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package nandcat.model.check;
+
+import nandcat.model.element.Circuit;
+
+/**
+ * Checks if any Ports has two Connections.
+ * 
+ * @version 0.1
+ */
+public class MultipleConnectionsCheck implements CircuitCheck {
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean isActive() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean setActive(boolean active) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean test(Circuit circuit) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void addListener(CheckListener l) {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void removeListener(CheckListener l) {
+        // TODO Auto-generated method stub
+    }
+}
