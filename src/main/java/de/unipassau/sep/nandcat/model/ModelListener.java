e6f714217f2d33ea604a37772e7401b61cffc9c0
Lorentz Roßbacher <lancetekk@googlemail.com>
2011-10-18 19:05:46 +0200

    interface erweitert

--- src/main/java/de/unipassau/sep/nandcat/model/ModelListener.java
+++ src/main/java/de/unipassau/sep/nandcat/model/ModelListener.java
@@ -10,7 +10,25 @@ public interface ModelListener {
 
     /**
      * Invoked when elements changes.
-     * @param e ModelEvent which holds changed elements and further information.
+     * 
+     * @param e
+     *            ModelEvent which holds changes.
      */
     void elementsChanged(ModelEvent e);
+
+    /**
+     * Invoked when checks changes.
+     * 
+     * @param e
+     *            ModelEvent which holds changes.
+     */
+    void checksChanged(ModelEvent e);
+
+    /**
+     * Invoked when checks changes.
+     * 
+     * @param e
+     *            ModelEvent which holds changes.
+     */
+    void simulationChanged(ModelEvent e);
 }
