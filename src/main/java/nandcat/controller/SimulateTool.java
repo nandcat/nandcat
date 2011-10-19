578453f677edb659a96850ed1a1d229c6d88bbad
Thomas Penteker <tek@serverop.de>
2011-10-19 23:48:38 +0200

    SimulateTool: implementiert neuen ModelListener

--- src/main/java/nandcat/controller/SimulateTool.java
+++ src/main/java/nandcat/controller/SimulateTool.java
@@ -2,12 +2,11 @@ package nandcat.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
 import java.util.List;
-import java.util.*;
-
+import java.util.Map;
 import javax.swing.ImageIcon;
 import javax.swing.plaf.basic.BasicComboBoxUI.ItemHandler;
-
 import nandcat.model.Model;
 import nandcat.model.ModelEvent;
 import nandcat.model.ModelListener;
@@ -21,128 +20,157 @@ import nandcat.view.CheckManager;
  */
 public class SimulateTool implements Tool {
 
-	/**
-	 * Current Model instance.
-	 */
-	private Model model;
+    /**
+     * Current Model instance.
+     */
+    private Model model;
 
-	/**
-	 * Current Controller instance.
-	 */
-	private Controller controller;
+    /**
+     * Current Controller instance.
+     */
+    private Controller controller;
 
-	/**
-	 * Current CheckManager instance.
-	 */
-	private CheckManager checkManager;
+    /**
+     * Current CheckManager instance.
+     */
+    private CheckManager checkManager;
 
-	/**
-	 * Icon representation of the Tool.
-	 */
-	private ImageIcon icon; // TODO icon setzen
+    /**
+     * Icon representation of the Tool.
+     */
+    private ImageIcon icon; // TODO icon setzen
 
-	/**
-	 * String representation of the Tool.
-	 */
-	private List<String> represent; // TODO beschreibung schreiben
+    /**
+     * String representation of the Tool.
+     */
+    private List<String> represent; // TODO beschreibung schreiben
 
-	/**
-	 * ActionListener of the Tool on the Buttons.
-	 */
-	private ActionListener buttonListener;
+    /**
+     * ActionListener of the Tool on the Buttons.
+     */
+    private ActionListener buttonListener;
 
-	/**
-	 * ModelListener of the Tool on the Model.
-	 */
-	private ModelListener modelListener;
+    /**
+     * ModelListener of the Tool on the Model.
+     */
+    private ModelListener modelListener;
 
-	/**
-	 * ItemHanlder of the Tool the the ComboBox in the CheckManager.
-	 */
-	private ItemHandler comboboxListener;
+    /**
+     * ItemHanlder of the Tool the the ComboBox in the CheckManager.
+     */
+    private ItemHandler comboboxListener;
 
-	/**
-	 * Constructs the SimulateTool.
-	 * 
-	 * @param controller
-	 *            Controller component of the application.
-	 */
-	public SimulateTool(Controller controller) {
-		this.controller = controller;
-		model = controller.getModel();
-	}
+    /**
+     * Constructs the SimulateTool.
+     * 
+     * @param controller
+     *            Controller component of the application.
+     */
+    public SimulateTool(Controller controller) {
+        this.controller = controller;
+        model = controller.getModel();
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public void setActive(boolean active) {
-		if (active) {
-			if (modelListener == null) {
-				modelListener = new ModelListener() {
+    /**
+     * {@inheritDoc}
+     */
+    public void setActive(boolean active) {
+        if (active) {
+            if (modelListener == null) {
+                modelListener = new ModelListener() {
 
-					public void elementsChanged(ModelEvent e) {
-					}
+                    public void elementsChanged(ModelEvent e) {
+                    }
 
-					public void checksChanged(ModelEvent e) {
-						// TODO Auto-generated method stub
+                    public void checksChanged(ModelEvent e) {
+                        // TODO Auto-generated method stub
+                    }
 
-					}
+                    public void simulationChanged(ModelEvent e) {
+                        // TODO Auto-generated method stub
+                    }
 
-					public void simulationChanged(ModelEvent e) {
-						// TODO Auto-generated method stub
+                    public void checksStarted(ModelEvent e) {
+                        // TODO Auto-generated method stub
+                    }
 
-					}
-				};
-			}
-			model.addListener(modelListener);
-		} else {
-			model.removeListener(modelListener);
-		}
-	}
+                    public void checksStopped(ModelEvent e) {
+                        // TODO Auto-generated method stub
+                    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public ActionListener getListener() {
-		if (buttonListener != null) {
-			return buttonListener;
-		} else {
-			buttonListener = new ActionListener() {
+                    public void simulationStarted(ModelEvent e) {
+                        // TODO Auto-generated method stub
+                    }
 
-				public void actionPerformed(ActionEvent e) {
-					// nur zum beispiel actionCommands sind defaultmäßig die
-					// namen der Buttons/menüpunkte
-					if (e.getActionCommand() == "checkstarting")
-						;
-					checkManager = new CheckManager(model.getChecks(), comboboxListener);
-				}
-			};
-		}
-		return buttonListener;
-	}
+                    public void simulationStopped(ModelEvent e) {
+                        // TODO Auto-generated method stub
+                    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public Map<String, ActionListener> getFunctionalities() {
-		buttonListener = new ActionListener() {
+                    public void importSucceeded(ModelEvent e) {
+                        // TODO Auto-generated method stub
+                    }
 
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
+                    public void importFailed(ModelEvent e) {
+                        // TODO Auto-generated method stub
+                    }
 
-			}
-		};
-		Map<String, ActionListener> map = new HashMap<String, ActionListener>();
-		for (String functionality : represent) {
-			map.put(functionality, buttonListener);
-		}
-		return map;
-	}
+                    public void exportSucceeded(ModelEvent e) {
+                        // TODO Auto-generated method stub
+                    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public ImageIcon getIcon() {
-		return icon;
-	}
+                    public void exportFailed(ModelEvent e) {
+                        // TODO Auto-generated method stub
+                    }
+                };
+            }
+            model.addListener(modelListener);
+        } else {
+            model.removeListener(modelListener);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public ActionListener getListener() {
+        if (buttonListener != null) {
+            return buttonListener;
+        } else {
+            buttonListener = new ActionListener() {
+
+                public void actionPerformed(ActionEvent e) {
+                    // nur zum beispiel actionCommands sind defaultmäßig die
+                    // namen der Buttons/menüpunkte
+                    if (e.getActionCommand() == "checkstarting")
+                        ;
+                    checkManager = new CheckManager(model.getChecks(), comboboxListener);
+                }
+            };
+        }
+        return buttonListener;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Map<String, ActionListener> getFunctionalities() {
+        buttonListener = new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+            }
+        };
+        Map<String, ActionListener> map = new HashMap<String, ActionListener>();
+        for (String functionality : represent) {
+            map.put(functionality, buttonListener);
+        }
+        return map;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public ImageIcon getIcon() {
+        return icon;
+    }
 }
