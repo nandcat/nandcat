62c40f72123133854459736c6bcab22c1f8f729e
Benedikt Lang <github@benediktlang.de>
2011-11-02 16:19:01 +0100

    Init ImportExport

---
+++ src/main/java/nandcat/model/XsdValidation.java
@@ -0,0 +1,44 @@
+package nandcat.model;
+
+import java.io.IOException;
+import javax.xml.transform.Source;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+
+/**
+ * Validation utility functions.
+ */
+public final class XsdValidation {
+
+    /**
+     * Private constructor for utility class.
+     */
+    private XsdValidation() {
+        throw new IllegalArgumentException();
+    }
+
+    /**
+     * Validates a given xml against serveral xsd schemas.
+     * 
+     * @param s
+     *            XML source to validate.
+     * @param asource
+     *            XSD sources.
+     * @param handler
+     *            Error handler.
+     * @throws SAXException
+     *             XML Exceptions.
+     * @throws IOException
+     *             File based exceptions.
+     */
+    public static void validate(Source s, Source[] asource, ErrorHandler handler) throws SAXException, IOException {
+        SchemaFactory schemafactory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
+        Schema schema = schemafactory.newSchema(asource);
+        Validator validator = schema.newValidator();
+        validator.setErrorHandler(handler);
+        validator.validate(s);
+    }
+}
---
+++ src/test/java/nandcat/model/importexport/XsdValidationTest.java
@@ -0,0 +1,56 @@
+package nandcat.model.importexport;
+
+import static org.junit.Assert.*;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import nandcat.Nandcat;
+import nandcat.NandcatTest;
+import nandcat.model.XsdValidation;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+public class XsdValidationTest {
+
+    private Source[] xsdSources;
+
+    private ErrorHandler throwingErrorHandler;
+
+    @Before
+    public void setUp() throws Exception {
+        xsdSources = new Source[] { new StreamSource(Nandcat.class.getResourceAsStream("../sepaf-extension.xsd")),
+                new StreamSource(Nandcat.class.getResourceAsStream("../circuits-1.0.xsd")) };
+        throwingErrorHandler = new ErrorHandler() {
+
+            public void warning(SAXParseException exception) throws SAXException {
+                throw exception;
+            }
+
+            public void fatalError(SAXParseException exception) throws SAXException {
+                throw exception;
+            }
+
+            public void error(SAXParseException exception) throws SAXException {
+                throw exception;
+            }
+        };
+    }
+
+    @Test(expected=SAXParseException.class)
+    public void testInvalidMissingEndTag() throws SAXException, IOException {
+        InputStream invalid1 = NandcatTest.class.getResourceAsStream("../formattest/sepaf-example-invalid-missingendtag.xml");
+        invalid1.available();
+        XsdValidation.validate(new StreamSource(invalid1), xsdSources, throwingErrorHandler);
+    }
+
+    @Test
+    public void test() throws SAXException, IOException {
+        InputStream valid = NandcatTest.class.getResourceAsStream("../formattest/sepaf-example-valid.xml");
+        valid.available();
+        XsdValidation.validate(new StreamSource(valid), xsdSources, throwingErrorHandler);
+    }
+}
