3bb0f6d230fbf011b034ea8df2047991f4e39656
Lorentz Roßbacher <lancetekk@googlemail.com>
2011-11-16 15:03:17 +0100

    flipflop neu gebaut - sollte a nichts ändern

--- src/main/java/nandcat/model/element/FlipFlop.java
+++ src/main/java/nandcat/model/element/FlipFlop.java
@@ -1,7 +1,5 @@
 package nandcat.model.element;
 
-import java.awt.Point;
-
 /**
  * FlipFlop implementation. A FlipFlop is used to "hold" a boolean state for a certain amount of time.
  */
@@ -17,18 +15,31 @@ public class FlipFlop extends Circuit {
      */
     public FlipFlop() {
         super("FlipFlop");
-        AndGate r = new AndGate(2, 2);
-        AndGate s = new AndGate(2, 2);
+        AndGate r = new AndGate();
+        AndGate s = new AndGate();
         NotGate rNot = new NotGate();
         NotGate sNot = new NotGate();
-        addModule(r, new Point());
-        addModule(s, new Point(1, 0));
-        addModule(rNot, new Point(0, 1));
-        addModule(sNot, new Point(1, 1));
+        IdentityGate idR = new IdentityGate();
+        IdentityGate idS = new IdentityGate();
+
+        addModule(r);
+        addModule(s);
+        addModule(rNot);
+        addModule(sNot);
+        addModule(idR);
+        addModule(idS);
+
+        // and -> not = nand
         addConnection(r.getOutPorts().get(0), rNot.getInPorts().get(0));
         addConnection(s.getOutPorts().get(0), sNot.getInPorts().get(0));
-        addConnection(rNot.getOutPorts().get(0), s.getInPorts().get(0));
-        addConnection(sNot.getOutPorts().get(0), r.getInPorts().get(0));
+
+        // nand -> ID
+        addConnection(rNot.getOutPorts().get(0), idR.getInPorts().get(0));
+        addConnection(sNot.getOutPorts().get(0), idS.getInPorts().get(0));
+
+        // ID -> significant other
+        addConnection(idR.getOutPorts().get(0), s.getInPorts().get(0));
+        addConnection(idS.getOutPorts().get(0), r.getInPorts().get(0));
     }
 
 }
