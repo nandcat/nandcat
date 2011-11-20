58453d2a1268cda071f950e99f40143d5f8ba735
Benedikt Lang <github@benediktlang.de>
2011-11-20 12:33:36 +0100

    Tests hinzugefügt.

--- src/test/java/nandcat/model/importexport/sepaf/SEPAFImporterSingleTest.java
+++ src/test/java/nandcat/model/importexport/sepaf/SEPAFImporterSingleTest.java
@@ -5,17 +5,23 @@ import static org.junit.Assert.fail;
 import java.awt.Point;
 import java.io.File;
 import java.io.IOException;
+
+import nandcat.model.Model;
 import nandcat.model.ModelElementDefaults;
 import nandcat.model.element.Circuit;
 import nandcat.model.element.Connection;
 import nandcat.model.element.Element;
 import nandcat.model.element.ImpulseGenerator;
 import nandcat.model.element.Module;
+import nandcat.model.element.NotGate;
+import nandcat.model.element.OrGate;
 import nandcat.model.element.factory.ModuleBuilder;
 import nandcat.model.element.factory.ModuleBuilderFactory;
 import nandcat.model.importexport.Exporter;
 import nandcat.model.importexport.Importer;
 import nandcat.view.StandardModuleLayouter;
+
+import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -36,6 +42,12 @@ public class SEPAFImporterSingleTest {
     private Circuit exportC;
 
     private ModuleBuilderFactory factory;
+
+    /**
+     * Class logger instance.
+     */
+    private static final Logger LOG = Logger
+            .getLogger(SEPAFImporterSingleTest.class);
 
     @Before
     public void setup() throws IOException {
@@ -215,7 +227,67 @@ public class SEPAFImporterSingleTest {
         c = importer.getCircuit();
         deepCompareCircuits(exportC, c);
         // String content = ImportExportUtil.getFileContent(file);
-        // System.out.println(content.replace(">", ">\n"));
+        // LOG.debug(content.replace(">", ">\n"));
+    }
+
+    @Test
+    public void testCircuit() {
+        ModuleBuilder builder = factory.getCircuitBuilder();
+        builder.setLocation(new Point(10, 20));
+        Circuit innerC = (Circuit) builder.build();
+        ModuleBuilder andGateB = factory.getAndGateBuilder();
+        andGateB.setLocation(new Point(20, 30));
+        innerC.addModule(andGateB.build());
+        exportC.addModule(innerC);
+        exporter.setCircuit(exportC);
+        exporter.exportCircuit();
+        assertTrue(importer.importCircuit());
+        assertTrue(importer.getCircuit() != null);
+        c = importer.getCircuit();
+        deepCompareCircuits(exportC, c);
+    }
+
+    @Test
+    public void testMultipleGates() {
+        ModuleBuilder notB = factory.getNotGateBuilder();
+        notB.setLocation(new Point(10, 20));
+        notB.setOutPorts(10);
+        ModuleBuilder orB = factory.getOrGateBuilder();
+        orB.setInPorts(4);
+        orB.setOutPorts(3);
+        orB.setLocation(new Point(20, 30));
+        NotGate not = (NotGate) notB.build();
+        OrGate or = (OrGate) orB.build();
+        exportC.addModule(or);
+        exportC.addModule(not);
+        exporter.setCircuit(exportC);
+        exporter.exportCircuit();
+        assertTrue(importer.importCircuit());
+        assertTrue(importer.getCircuit() != null);
+        c = importer.getCircuit();
+        deepCompareCircuits(exportC, c);
+    }
+
+    @Test
+    public void testConnection() {
+        ModuleBuilder notB = factory.getNotGateBuilder();
+        notB.setLocation(new Point(10, 20));
+        notB.setOutPorts(10);
+        ModuleBuilder orB = factory.getOrGateBuilder();
+        orB.setInPorts(4);
+        orB.setOutPorts(3);
+        orB.setLocation(new Point(20, 30));
+        NotGate not = (NotGate) notB.build();
+        OrGate or = (OrGate) orB.build();
+        exportC.addConnection(not.getOutPorts().get(0), or.getInPorts().get(0));
+        exportC.addModule(or);
+        exportC.addModule(not);
+        exporter.setCircuit(exportC);
+        exporter.exportCircuit();
+        assertTrue(importer.importCircuit());
+        assertTrue(importer.getCircuit() != null);
+        c = importer.getCircuit();
+        deepCompareCircuits(exportC, c);
     }
 
     @After
@@ -226,7 +298,8 @@ public class SEPAFImporterSingleTest {
 
     private void deepCompareCircuits(Circuit expected, Circuit actual) {
         if (!expected.getUuid().equals(actual.getUuid())) {
-            fail("Uuid differs: " + expected.getUuid() + " actual: " + actual.getUuid());
+            fail("Uuid differs: " + expected.getUuid() + " actual: "
+                    + actual.getUuid());
         } else {
             if (expected.getElements().size() != actual.getElements().size()) {
                 fail("Circuits differ in amount of elements");
@@ -234,42 +307,55 @@ public class SEPAFImporterSingleTest {
             for (Element actualElement : actual.getElements()) {
                 boolean found = false;
                 for (Element expectedElement : expected.getElements()) {
-                    if (actualElement.getClass().getName().equals(expectedElement.getClass().getName())) {
+                    if (actualElement.getClass().getName()
+                            .equals(expectedElement.getClass().getName())) {
                         if (actualElement instanceof Module) {
-                            if (((Module) actualElement).getRectangle().x == ((Module) expectedElement).getRectangle().x
+                            if (((Module) actualElement).getRectangle().x == ((Module) expectedElement)
+                                    .getRectangle().x
                                     && ((Module) actualElement).getRectangle().y == ((Module) expectedElement)
                                             .getRectangle().y) {
                                 // Found element at same position.
-                                if (deepCompareModules((Module) expectedElement, (Module) actualElement)) {
+                                if (deepCompareModules(
+                                        (Module) expectedElement,
+                                        (Module) actualElement)) {
                                     found = true;
                                 } else {
-                                    System.out.println("Similar element found: " + (Module) actualElement);
+                                    System.out
+                                            .println("Similar element found: "
+                                                    + (Module) actualElement);
                                 }
                             } else {
-                                System.out.println("coords not right");
+                                LOG.debug("coords not right");
                             }
                         } else if (actualElement instanceof Connection) {
-                            if (deepCompareConnections((Connection) expectedElement, (Connection) actualElement)) {
+                            if (deepCompareConnections(
+                                    (Connection) expectedElement,
+                                    (Connection) actualElement)) {
                                 found = true;
                             }
                         }
                     }
                 }
                 if (!found) {
-                    fail("Element " + actualElement + " not found in expectations");
+                    fail("Element " + actualElement
+                            + " not found in expectations");
                 }
             }
         }
     }
 
-    private boolean deepCompareConnections(Connection expected, Connection actual) {
-        if (!deepCompareModules(expected.getNextModule(), actual.getNextModule())) {
-            System.out.println("Next Module not similar");
+    private boolean deepCompareConnections(Connection expected,
+            Connection actual) {
+        LOG.debug("Check connection");
+        if (!deepCompareModules(expected.getNextModule(),
+                actual.getNextModule())) {
+            LOG.debug("Next Module not similar");
             return false;
         }
 
-        if (!deepCompareModules(expected.getPreviousModule(), actual.getPreviousModule())) {
-            System.out.println("Previous Module not similar");
+        if (!deepCompareModules(expected.getPreviousModule(),
+                actual.getPreviousModule())) {
+            LOG.debug("Previous Module not similar");
             return false;
         }
         return true;
@@ -277,34 +363,37 @@ public class SEPAFImporterSingleTest {
     }
 
     private boolean deepCompareModules(Module expected, Module actual) {
-        if (!(((Module) actual).getRectangle().x == ((Module) expected).getRectangle().x && ((Module) actual)
-                .getRectangle().y == ((Module) expected).getRectangle().y)) {
-            System.out.println("Coordinates of modules differ");
+        if (!(((Module) actual).getRectangle().x == ((Module) expected)
+                .getRectangle().x && ((Module) actual).getRectangle().y == ((Module) expected)
+                .getRectangle().y)) {
+            LOG.debug("Coordinates of modules differ");
             return false;
         }
 
         if (expected.getInPorts().size() != actual.getInPorts().size()) {
-            System.out.println("InPorts differ");
+            LOG.debug("InPorts differ");
             return false;
         }
 
         if (expected.getOutPorts().size() != actual.getOutPorts().size()) {
-            System.out.println("OutPorts differ");
-            System.out.println("Expected: " + expected.getOutPorts().size());
-            System.out.println("Actual: " + actual.getOutPorts().size());
+            LOG.debug("OutPorts differ");
+            LOG.debug("Expected: " + expected.getOutPorts().size());
+            LOG.debug("Actual: " + actual.getOutPorts().size());
             return false;
         }
         if (expected.getName() == null ^ actual.getName() == null) {
-            System.out.println("One name is null");
+            LOG.debug("One name is null");
             return false;
         }
-        if (expected.getName() != null && !expected.getName().equals(actual.getName())) {
-            System.out.println("Name differs");
+        if (expected.getName() != null
+                && !expected.getName().equals(actual.getName())) {
+            LOG.debug("Name differs");
             return false;
         }
         if (expected instanceof ImpulseGenerator) {
-            if (((ImpulseGenerator) expected).getFrequency() != ((ImpulseGenerator) actual).getFrequency()) {
-                System.out.println("Frequency differs");
+            if (((ImpulseGenerator) expected).getFrequency() != ((ImpulseGenerator) actual)
+                    .getFrequency()) {
+                LOG.debug("Frequency differs");
                 return false;
             }
         }
