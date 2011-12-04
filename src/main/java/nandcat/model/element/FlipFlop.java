06bcd68a0a58268432b6fefa8ccf2b7b14efef3c
Lorentz Roßbacher <lancetekk@googlemail.com>
2011-12-04 18:26:32 +0100

    lampen, schalter hinzugefügt

--- src/main/java/nandcat/model/element/FlipFlop.java
+++ src/main/java/nandcat/model/element/FlipFlop.java
@@ -21,6 +21,10 @@ public class FlipFlop extends Circuit {
         NotGate sNot = new NotGate(1);
         IdentityGate idR = new IdentityGate(2);
         IdentityGate idS = new IdentityGate(2);
+        ImpulseGenerator impy = new ImpulseGenerator(0);
+        ImpulseGenerator impz = new ImpulseGenerator(0);
+        Lamp licht = new Lamp();
+        Lamp scheffel = new Lamp();
 
         addModule(r);
         addModule(s);
@@ -28,6 +32,10 @@ public class FlipFlop extends Circuit {
         addModule(sNot);
         addModule(idR);
         addModule(idS);
+        addModule(impy);
+        addModule(impz);
+        addModule(licht);
+        addModule(scheffel);
 
         // and -> not = nand
         addConnection(r.getOutPorts().get(0), rNot.getInPorts().get(0));
@@ -40,6 +48,14 @@ public class FlipFlop extends Circuit {
         // ID -> significant other
         addConnection(idR.getOutPorts().get(0), s.getInPorts().get(0));
         addConnection(idS.getOutPorts().get(0), r.getInPorts().get(0));
+
+        // Imp -> not
+        addConnection(impy.getOutPorts().get(0), rNot.getInPorts().get(0));
+        addConnection(impz.getOutPorts().get(0), sNot.getInPorts().get(0));
+
+        // id -> lamp
+        addConnection(idR.getOutPorts().get(1), licht.getInPorts().get(0));
+        addConnection(idS.getOutPorts().get(1), scheffel.getInPorts().get(0));
     }
 
 }
