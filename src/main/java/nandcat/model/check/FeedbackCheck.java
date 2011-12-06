b2b5b8b9728ad5e582843559c2da3eecb1a7a223
Veronika Scholz <veronikascholz@gmail.com>
2011-12-07 00:42:27 +0100

    FeedbackCheck funktioniert bei FlipFlops und Halbaddierern und hoffentlich auch bei allen anderen Schaltkreisen

--- src/main/java/nandcat/model/check/FeedbackCheck.java
+++ src/main/java/nandcat/model/check/FeedbackCheck.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import nandcat.model.check.CheckEvent.State;
 import nandcat.model.element.Circuit;
 import nandcat.model.element.Element;
+import nandcat.model.element.IdentityGate;
 import nandcat.model.element.Module;
 import nandcat.model.element.Port;
 
@@ -71,6 +72,9 @@ public class FeedbackCheck implements CircuitCheck {
                     // If the test fails add the Module which caused the fail to the CheckEvent.
                     elements.add(current.getModule());
                     informListeners(State.FAILED, elements);
+                    for (Element e : elements) {
+                        System.out.println(e);
+                    }
                     return false;
                 }
                 visited.add(current);
@@ -79,14 +83,66 @@ public class FeedbackCheck implements CircuitCheck {
                  * When the port has a connection the outgoing ports of the following module are added to the queue. If
                  * there is no further connection an ending module of the circuit is reached. If there are still
                  * elements in the queue the algorithm starts from the beginning and the visited set has to be reset.
+                 * SPECIAL TREATMENT: If there's an identityGate the algorithm looks at both paths outgoing from the
+                 * identity separately.
                  */
                 if (current.getConnection() != null) {
-                    q.addAll(current.getConnection().getOutPort().getModule().getOutPorts());
+                    if (current.getModule() instanceof IdentityGate) {
+                        for (Port p : current.getConnection().getNextModule().getOutPorts()) {
+                            Set<Element> els = findFeedback(p, visited, elements);
+                            if (!els.isEmpty()) {
+                                elements.addAll(els);
+                                informListeners(State.FAILED, elements);
+                                return false;
+                            }
+                        }
+                    } else {
+                        q.addAll(current.getConnection().getOutPort().getModule().getOutPorts());
+                    }
                 }
             }
         }
         informListeners(State.SUCCEEDED, elements);
         return true;
+    }
+
+    /**
+     * This helper calls the FeedbackCheck recursively when there's an identity gate.
+     * 
+     * @param port
+     *            OutPort of the identity gate's following module. The first path starts from here.
+     * @param v
+     *            Set of Ports already visited previously.
+     * @param elements
+     *            Set of Elements containing the elements causing the check to fail.
+     * @return Set of Elements which cause the check to fail. If the check was successful the set is empty.
+     */
+    private Set<Element> findFeedback(Port port, Set<Port> v, Set<Element> elements) {
+        Queue<Port> q = new LinkedList<Port>();
+        // If the algorithm goes deeper it must use the unmodified visited set to avoid duplicates.
+        Set<Port> visited = new HashSet<Port>();
+        visited.addAll(v);
+        q.add(port);
+        while (!q.isEmpty()) {
+            Port current = q.poll();
+            if (visited.contains(current)) {
+
+                // If the test fails add the Module which caused the fail.
+                elements.add(current.getModule());
+                return elements;
+            }
+            visited.add(current);
+            if (current.getConnection() != null) {
+                if (current.getModule() instanceof IdentityGate) {
+                    for (Port p : current.getConnection().getNextModule().getOutPorts()) {
+                        findFeedback(p, v, elements);
+                    }
+                } else {
+                    q.addAll(current.getConnection().getOutPort().getModule().getOutPorts());
+                }
+            }
+        }
+        return elements;
     }
 
     /**
