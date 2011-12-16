ffaad5d2613355c029053491a8379788480043fd
Benedikt Lang <github@benediktlang.de>
2011-12-16 14:06:48 +0100

    Abgabestruktur schoener. Log4j angepasst. Pom baut version 1.0

--- src/main/java/nandcat/Nandcat.java
+++ src/main/java/nandcat/Nandcat.java
@@ -42,7 +42,7 @@ public final class Nandcat {
         Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
 
             public void uncaughtException(Thread t, Throwable e) {
-                LOG.error("Exception caught in Thread: " + t.toString(), e);
+                LOG.warn("Exception caught in Thread: " + t.toString(), e);
             }
         });
         setLocales(args);
